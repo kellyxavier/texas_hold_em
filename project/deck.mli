285267b00fd25a11fa6c2f53cfa10ffdecf430f5
Valeria Marques <vmm49@cornell.edu>
2019-10-27 17:13:17 -0400

    Add deck interface

--- project/deck.mli
+++ project/deck.mli
@@ -1,1 +1,19 @@
-TO DO
+(** 
+    Representation of a deck of cards.
+*)
+
+(** The type of a card suit. *)
+type suit = Clubs | Diamonds | Hearts | Spades
+
+(** The type of a card. *)
+type card = (suit * int)
+
+(** The type of a deck. *)
+type deck = card list
+
+(** [shuffle] is a randomized deck of 52 cards without duplicates. *)
+val shuffle : deck
+
+(** [draw_card n d] is a list of [n] cards drawn from [d]. 
+    Raises [EmptyDeck] if [n] is greater than the number of cards in [d]. *)
+val draw_card : int -> deck -> card list
