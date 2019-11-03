2d16a8b56fdf1b276ed99f1b0e023758666898c7
Kelly Xavier <kgx2@cornell.edu>
2019-11-03 14:01:20 -0500

    tests work with functors

--- project/test.ml
+++ project/test.ml
@@ -1,30 +1,8 @@
 open OUnit2
-open Deck
-open Player
 
 open Test_d
 open Test_k
 open Test_v
-
-let p1 = create_player "kelly"
-
-let deck_tests =
-  [
-
-  ]
-
-let player_tests =
-  [
-    "tests player's hand is empty " >:: (fun _ -> 
-        assert_equal empty (p1 |> hand));
-
-  ]
-
-let suite =
-  "test suite for A2"  >::: List.flatten [
-    deck_tests;
-    player_tests;
-  ]
 
 let tests_merged = 
   List.flatten [
@@ -32,5 +10,8 @@ let tests_merged =
     Test_k.tests;
     Test_v.tests
   ]
+
+let suite = 
+  "test suite for project" >::: tests_merged
 
 let _ = run_test_tt_main suite
--- project/test_k.ml
+++ project/test_k.ml
@@ -1,1 +1,18 @@
-let tests = []
+open OUnit2
+open Deck
+open Player
+
+
+let p1 = create_player "kelly"
+
+let player_tests =
+  [
+    "tests player's hand is empty " >:: (fun _ -> 
+        assert_equal empty (p1 |> hand));
+
+  ]
+
+let tests =
+  List.flatten [
+    player_tests;
+  ]
