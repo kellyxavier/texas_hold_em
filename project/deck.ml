7352aafb2893899241ab9f50d8de0ad5c95339f0
Valeria Marques <vmm49@cornell.edu>
2019-11-03 09:11:38 -0500

    Implement deck.ml
    - Implemented shuffle and draw_card functions in Deck
    - Adjusted interface slightly to account for better implementation

--- project/deck.ml
+++ project/deck.ml
@@ -1,11 +1,62 @@
+(** [EmptyDeck] is the exception raised when actions are performed on a deck
+    without enough cards to complete it. *)
+exception EmptyDeck
+
 type suit = Clubs | Diamonds | Hearts | Spades
 
 type card = (suit * int)
 
 type deck = card list
 
-let shuffle d =
-  failwith "unimplemented"
+(** [standard_deck] is an ordered list of all 52 cards, going from clubs
+    between Ace and King, then Diamonds, then Hearts, and then Spades. *)
+let standard_deck = [
+  (Clubs, 1); (Clubs, 2); (Clubs, 3); (Clubs, 4); (Clubs, 5); (Clubs, 6);
+  (Clubs, 7); (Clubs, 8); (Clubs, 9); (Clubs, 10); (Clubs, 11); (Clubs, 12);
+  (Clubs, 13); (Diamonds, 1); (Diamonds, 2); (Diamonds, 3); (Diamonds, 4);
+  (Diamonds, 5); (Diamonds, 6); (Diamonds, 7); (Diamonds, 8); (Diamonds, 9); 
+  (Diamonds, 10); (Diamonds, 11); (Diamonds, 12); (Diamonds, 13); (Hearts, 1);
+  (Hearts, 2); (Hearts, 3); (Hearts, 4); (Hearts, 5); (Hearts, 6); (Hearts, 7); 
+  (Hearts, 8); (Hearts, 9); (Hearts, 10); (Hearts, 11); (Hearts, 12); 
+  (Hearts, 13); (Spades, 1); (Spades, 2); (Spades, 3); (Spades, 4); (Spades, 5);
+  (Spades, 6); (Spades, 7); (Spades, 8); (Spades, 9); (Spades, 10); 
+  (Spades, 11); (Spades, 12); (Spades, 13)
+  ]
+
+(** [get_card n d r] returns a tuple of the [n]th card in [d] and [r] which is
+    [d] without the [n]th card. [r] might not be in same order as [d] was.
+    Raises [EmptyDeck] is [n] is greater than the length of [d]. *)
+let rec get_card n d r =
+  if n > 0
+  then
+    match d with
+    | [] -> raise EmptyDeck
+    | h :: t -> get_card (n - 1) t (h :: r)
+  else
+    match d with
+    | [] -> raise EmptyDeck
+    | h :: t -> (h, List.rev_append r t)
+
+(** [shuffle_help n d lst] is a tail-recursive implmenentation of shuffle
+    which returns [lst]. *)
+let rec shuffle_help n d lst =
+  if n > 0
+  then
+    match get_card (Random.int n) d [] with
+    | (c, r) -> shuffle_help (n - 1) r (c :: lst)
+  else lst
+
+let shuffle =
+  shuffle_help 52 standard_deck []
+
+(** [draw_card_help n d lst] is a tail-recursive implementation of draw_card
+    which returns [lst]. *)
+let rec draw_card_help n d lst =
+  if n > 0 
+  then match d with
+  | [] -> raise EmptyDeck
+  | h :: t -> draw_card_help (n - 1) t (h :: lst)
+  else lst
 
 let draw_card n d =
-  failwith "unimplemented"
+  draw_card_help n d []
--- project/deck.mli
+++ project/deck.mli
@@ -5,15 +5,16 @@
 (** The type of a card suit. *)
 type suit = Clubs | Diamonds | Hearts | Spades
 
-(** The type of a card. *)
+(** The type of a card. The second field int represents the rank of the card,
+    with the Jack being 11, Queen being 12, King being 13, and Ace is 1. *)
 type card = (suit * int)
 
 (** The type of a deck. *)
 type deck = card list
 
 (** [shuffle] is a randomized deck of 52 cards without duplicates. *)
-val shuffle : deck -> deck
+val shuffle : deck
 
-(** [draw_card n d] is a list of [n] cards drawn from [d]. 
+(** [draw_card n d] is a list of [n] cards that are not in [d]. 
     Raises [EmptyDeck] if [n] is greater than the number of cards in [d]. *)
-val draw_card : int -> deck -> card list
+val draw_card : int -> deck -> deck
