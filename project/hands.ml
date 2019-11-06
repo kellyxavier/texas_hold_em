812d1a74971168a0d24a520bfce4d04b91782eee
Valeria Marques <vmm49@cornell.edu>
2019-11-05 20:31:51 -0500

    Add some tests for rank_hand

--- project/hands.ml
+++ project/hands.ml
@@ -1,5 +1,86 @@
 open Deck
 
+(*================HIGH VALUE HANDS================*)
+
+let hv_table =
+  empty 
+  |> insert Clubs 2
+  |> insert Clubs 3
+  |> insert Diamonds 5
+  |> insert Spades 6
+  |> insert Hearts 8
+
+let hv_low =
+  hv_table
+  |> insert Spades 10
+  |> insert Spades 11
+
+let hv_high =
+  hv_table
+  |> insert Spades 12
+  |> insert Spades 13
+
+let hv_ace =
+  hv_table
+  |> insert Spades 9
+  |> insert Spades 1
+
+(*================PAIR HANDS================*)
+
+let pair_table =
+  empty 
+  |> insert Clubs 2
+  |> insert Clubs 3
+  |> insert Diamonds 1
+  |> insert Spades 6
+  |> insert Hearts 8
+
+let pair_low =
+  pair_table
+  |> insert Diamonds 2
+  |> insert Hearts 10
+
+let pair_high =
+  pair_table
+  |> insert Diamonds 13
+  |> insert Clubs 13
+
+let pair_ace =
+  pair_table
+  |> insert Spades 1
+  |> insert Clubs 1
+
+let pair_equal_low =
+  pair_table
+  |> insert Hearts 6
+  |> insert Spades 2
+
+let pair_equal_high =
+  pair_table
+  |> insert Spades 6
+  |> insert Hearts 13
+
+(*================TWO PAIR TESTS================*)
+
+let tpair_table = pair_table
+
+let tpair_low =
+  tpair_table
+  |> insert Diamonds 2
+  |> insert Hearts 3
+
+let tpair_high =
+  tpair_table
+  |> insert Diamonds 8
+  |> insert Clubs 6
+
+let tpair_ace =
+  tpair_table
+  |> insert Diamonds 1
+  |> insert Spades 2
+
+(*================FLUSH HANDS================*)
+
 let flush_table =
   empty
   |> insert Hearts 2
@@ -8,17 +89,29 @@ let flush_table =
   |> insert Clubs 7
   |> insert Clubs 10
 
+let flush_low =
+  flush_table
+  |> insert Hearts 12
+  |> insert Hearts 5
+
+let flush_high =
+  flush_table
+  |> insert Hearts 13
+  |> insert Hearts 11
+
 let flush_ace =
   flush_table 
   |> insert Hearts 1
   |> insert Hearts 12
 
-let flush_king =
-  flush_table
-  |> insert Hearts 13
-  |> insert Hearts 11
+(*================ROYAL FLUSH HANDS================*)
 
-let flush_queen =
-  flush_table
-  |> insert Hearts 12
-  |> insert Hearts 5
+let royal_flush =
+  empty
+  |> insert Spades 8
+  |> insert Spades 9
+  |> insert Spades 10
+  |> insert Spades 11
+  |> insert Spades 12
+  |> insert Spades 13
+  |> insert Spades 1
--- project/test_d.ml
+++ project/test_d.ml
@@ -3,8 +3,58 @@ open Rank_hand
 open Hands
 
 let tests = [
+
+  (*================HIGH VALUE TESTS================*)
+
+  "a higher-card hand beats a lower one" >:: (fun _ -> 
+      assert_equal true (hand_value hv_low < hand_value hv_high));
+
+  "an ace beats all high-value" >:: (fun _ -> 
+      assert_equal true (hand_value hv_high < hand_value hv_ace));
+
+  (*================PAIR TESTS================*)
+
+  "a higher pair beats a lower pair" >:: (fun _ ->
+      assert_equal true (hand_value pair_low < hand_value pair_high));
+
+  "an ace beats all pairs" >:: (fun _ ->
+      assert_equal true (hand_value pair_high < hand_value pair_ace));
+
+  "the same pair will go to the higher-valued hand" >:: (fun _ ->
+      assert_equal true 
+      (hand_value pair_equal_low < hand_value pair_equal_high));
+
+  "pair beats high-valued hand" >:: (fun _ ->
+      assert_equal true (hand_value hv_ace < hand_value pair_low));
+
+  (*================TWO PAIR TESTS================*)
+
+  "a higher two pair beats a lower two pair" >:: (fun _ ->
+      assert_equal true (hand_value tpair_low < hand_value tpair_high));
+
+  "an ace beats all two pairs" >:: (fun _ ->
+      assert_equal true (hand_value tpair_high < hand_value tpair_ace));
+
+  "two pair beats high-valued hand" >:: (fun _ ->
+      assert_equal true (hand_value hv_ace < hand_value tpair_low));
+
+  "two pair beats pair hand" >:: (fun _ ->
+      assert_equal true (hand_value pair_ace < hand_value tpair_low));
+
+  (*================FLUSH TESTS================*)
+
   "flush with a king beats a flush with a lower high card" >:: (fun _ -> 
-      assert_equal true (hand_value flush_queen < hand_value flush_king));
-  (*  "flush with an ace beats a flush with a lower high card" >:: (fun _ -> 
-       assert_equal true (hand_value flush_king < hand_value flush_ace)); *)
+      assert_equal true (hand_value flush_low < hand_value flush_high));
+
+  "flush with an ace beats a flush with a lower high card" >:: (fun _ -> 
+      assert_equal true (hand_value flush_high < hand_value flush_ace));
+  
+  "flush beats high-valued hand" >:: (fun _ ->
+      assert_equal true (hand_value hv_ace < hand_value flush_low));
+
+  (*================ROYAL FLUSH TESTS================*)
+
+  "royal flush beats all" >:: (fun _ ->
+      assert_equal true (hand_value flush_ace < hand_value royal_flush));
+
 ]
