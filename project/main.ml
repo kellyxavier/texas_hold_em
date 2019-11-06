1864320b566031dd5024080688e997a5cbbeadf1
Kelly Xavier <kgx2@cornell.edu>
2019-11-05 23:05:09 -0500

    first milestone done

--- project/main.ml
+++ project/main.ml
@@ -1,6 +1,8 @@
 open Deck
 open Player
 open State
+open Hands
+open Rank_hand
 
 (** [create_player_list x acc] is the player list consisting of [x] players*)
 let rec create_player_list x acc =
@@ -29,11 +31,51 @@ let rec show_info players =
       begin match read_line () with 
         |_ -> ANSITerminal.erase Screen; show_info t end
 
+let comparing_hands_demo unit =
+  print_endline "Now, let's compare some hands to see who wins!";
+  print_endline "Let's start off by comparing ";
+  (*First should win*)
+  print_endline (to_string hv_ace);
+  print_endline (to_string hv_high);
+  if (hand_value hv_ace > hand_value hv_high)
+  then print_endline "First hand wins\n\n"
+  else (print_endline "Second hand wins\n\n");
+
+  (*Second should win*)
+  print_endline (to_string pair_ace);
+  print_endline (to_string tpair_low);
+  if (hand_value pair_ace > hand_value tpair_low)
+  then print_endline "First hand wins\n\n"
+  else (print_endline "Second hand wins\n\n");
+
+  (*First should win*)
+  print_endline (to_string foak_high);
+  print_endline (to_string foak_low);
+  if (hand_value foak_high > hand_value foak_low)
+  then print_endline "First hand wins\n\n"
+  else (print_endline "Second hand wins\n\n");
+
+  (*Second should win*)
+  print_endline (to_string toak_ace);
+  print_endline (to_string sf_low);
+  if (hand_value toak_ace > hand_value sf_low)
+  then print_endline "First hand wins\n\n"
+  else (print_endline "Second hand wins\n\n");
+
+  (*Second should win*)
+  print_endline (to_string toak_ace);
+  print_endline (to_string royal_flush);
+  if (hand_value toak_ace > hand_value royal_flush)
+  then print_endline "First hand wins\n\n"
+  else (print_endline "Second hand wins\n\n");
+  print_endline "\n\nThanks for playing our demo! See ya next week!"
+
 (** [start_game x] initializes a game with [x] players*)
 let start_game x =
   let player_list = create_player_list x [] in
   let st = new_round player_list in let active_players = active_players st in 
-  show_info active_players; print_endline "That's all folks!"
+  show_info active_players; print_endline "That's all for the demo game folks!";
+  comparing_hands_demo ()
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
