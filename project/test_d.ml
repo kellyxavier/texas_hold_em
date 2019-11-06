c12b847aceaf950da434be616488600e0d943ed2
Valeria Marques <vmm49@cornell.edu>
2019-11-05 19:17:38 -0500

    Fix shuffle

--- project/test_d.ml
+++ project/test_d.ml
@@ -5,6 +5,6 @@ open Hands
 let tests = [
   "flush with a king beats a flush with a lower high card" >:: (fun _ -> 
       assert_equal true (hand_value flush_queen < hand_value flush_king));
-  (* "flush with an ace beats a flush with a lower high card" >:: (fun _ -> 
+(*  "flush with an ace beats a flush with a lower high card" >:: (fun _ -> 
      assert_equal true (hand_value flush_king < hand_value flush_ace)); *)
 ]
