e9297176ddf592edf3bd77e090b58e4372aeccac
David Siher <das568@cornell.edu>
2019-12-07 15:56:14 -0500

    Began documentation for hands.mli

--- project/hands.mli
+++ project/hands.mli
@@ -2,40 +2,81 @@ open Deck
 
 (*================PARTIAL HANDS (for AI)==========*)
 (*================HIGH CARD HANDS=================*)
+(** [hc_high] is the hand containing only a high card (King) *)
 val hc_high : deck
+
+(** [hc_ace] is the hand containing only an ace *)
 val hc_ace : deck
+
+(** [hc_low] is the hand containing only a low card (Two) *)
 val hc_low : deck
 
 (*===================PAIR HANDS===================*)
+(** [low_pair] is the hand containing only a low pair (Pair of Twos) *)
 val low_pair : deck
+
+(** [high_pair] is the hand containing only a high pair (Pair of Kings) *)
 val high_pair : deck
+
+(** [ace_pair] is the hand containing only an ace pair *)
 val ace_pair : deck
 
 (*==================TRIO HANDS====================*)
+(** [low_trio] is the hand containing only a low trio (Trio of Twos) *)
 val low_trio : deck
+
+(** [high_trio] is the hand containing only a high trio (Trio of Kings) *)
 val high_trio : deck
+
+(** [ace_trio] is the hand containing only an ace trio *)
 val ace_trio : deck
 
 (*================FULL HANDS BELOW================*)
 (*================HIGH VALUE HANDS================*)
-(** [hv_table] is the table upon which we will test high value hands*)
+(** [hv_table] is the base table to test high value hands*)
 val hv_table : deck
+
+(** [hv_low] is the hand consisting of low cards (Ten and Eleven) added to 
+    the base table*)
 val hv_low : deck
+
+(** [hv_high] is the hand consisting of high cards (Queen and King) added to 
+    the base table*)
 val hv_high : deck
+
+(** [hv_ace] is the hand consisting of an ace and other card (Ace and Nine) 
+    added to the base table*)
 val hv_ace : deck
 
 (*================PAIR HANDS================*)
-
+(** [pair_table] is the base table to test pair hands*)
 val pair_table : deck
+
+(** [pair_low] is the hand consisting of an additional 2 (creating a pair)
+    and an extra ten which is counted as a high card added to the base table. *)
 val pair_low : deck
+
+(** [pair_high] is the hand consisting of two kings (creating a pair)
+    added to the base table. *)
 val pair_high : deck
+
+(** [pair_ace] is the hand consisting of an ace (creating a pair) and an
+    additional 9 which is counted as a high card added to the base table. *)
 val pair_ace : deck
+
+(** [pair_equal_low] is the hand consisting of a 6 (creating a pair) and a low
+    card (Four) added to the base table. *)
 val pair_equal_low : deck
+
+(** [pair_equal_high] is the hand consisting of a 6 (creating a pair) and a high
+    card (King) added to the base table. *)
 val pair_equal_high : deck
 
 (*================TWO PAIR HANDS================*)
-
+(** [tpair_table] is the base table to test two pair hands *)
 val tpair_table : deck
+
+
 val tpair_low : deck
 val tpair_high : deck
 val tpair_ace : deck
