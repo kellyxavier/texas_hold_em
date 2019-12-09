8ce44684f4f8b868e508b4d699b3fae640f9462c
David Siher <das568@cornell.edu>
2019-12-09 16:34:42 -0500

    Added Test Plan

--- project/test.ml
+++ project/test.ml
@@ -1,3 +1,22 @@
+(** We split out tests amongst three separate files that are all merged into one
+    suite in test.ml so that we could each add tests as needed without the need
+    to worry about merge conflicts.
+
+    Our general unit testing strategy was glass box, we would try out functions
+    that the game would use based on the possible values we knew it would need
+    to take in (making sure to use edge cases whenever they existed)
+    to make sure that it would handle the input it would be given by
+    the game as we expected it to. This worked for the modules with simple
+    functions (state, rank_hand, etc.) but when it came to the modules with 
+    functions that took terminal input (main) we resorted to playtesting 
+    in order to find any anomalies and bugs. 
+
+    In the event that a bug was found in a simple function during playtesting,
+    a test case was created to reflect that bug and added to our suite, 
+    whereupon we could then make sure that it was fixed in future changes to our
+    project. *)
+
+
 open OUnit2
 
 open Test_d
